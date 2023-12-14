ffe2eb382c9e157146ae33367f5ed4f78db3fd9b
Ano2225 <ouatt0767@gmail.com>
2023-12-14 17:17:47 +0000

    Ut et Login Page responsie geré
    Ut la page des historiques de paris.

--- login.php
+++ login.php
@@ -3,56 +3,59 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Administrateur Login</title>
   <style>
     body {
-      background-color: #f5f5f5;
       font-family: Arial, sans-serif;
-        display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-    
+      margin: 0;
+      padding: 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+      background-color: #f5f5f5;
     }
 
     form {
-      margin: 0 auto;
-      width: 300px;
-      border: 1px solid #ddd;
-      border-radius: 5px;
+      width: 80%;
+      max-width: 400px;
       padding: 20px;
       background-color: #fff;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
     }
 
-    label {
+    label,
+    input,
+    button {
       display: block;
-      margin-bottom: 5px;
-      font-weight: bold;
+      width: 100%;
+      margin-bottom: 10px;
     }
 
-    input[type="text"],
-    input[type="password"] {
-      width: 100%;
-      padding: 5px;
-      border: 1px solid #ccc;
-      border-radius: 3px;
+    input,
+    button {
+      padding: 10px;
       font-size: 16px;
+      border: 1px solid #ccc;
+      border-radius: 4px;
     }
 
     button {
-      margin-top: 10px;
-      background-color: #4CAF50;
+      background-color: #007bff;
       color: #fff;
-      border: none;
-      padding: 10px 20px;
-      font-size: 16px;
-      border-radius: 5px;
       cursor: pointer;
+      transition: background-color 0.3s ease;
     }
 
     button:hover {
-      background-color: #3E8E41;
+      background-color: #0056b3;
+    }
+
+    @media screen and (max-width: 768px) {
+      form {
+        width: 85%;
+      }
     }
   </style>
 </head>
@@ -60,10 +63,8 @@
   <form action="loginn.php" method="post">
     <label for="username">Nom d'utilisateur :</label>
     <input type="text" name="username" required>
-    <br>
     <label for="password">Mot de passe :</label>
     <input type="password" name="password" required>
-    <br>
     <button type="submit">Se connecter</button>
   </form>
 </body>
